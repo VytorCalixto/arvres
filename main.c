b92eef1946d9eff805a22540fb66de0fdc05cc82
Vytor Calixto <vytorcalixto@gmail.com>
2015-05-25 23:22:44 +0000

    Início da implementação da busca

--- main.c
+++ main.c
@@ -13,6 +13,9 @@ int main (int argc, char **argv) {
     ApAVL raizAVL = NULL;
 
     FILE *dados = fopen(registros, "r");
+    if(dados == NULL){
+        exit(-1);
+    }
     int codigo = 0;
     char jogo;
     int linha = 0;
@@ -30,11 +33,38 @@ int main (int argc, char **argv) {
         fscanf(dados, "%d", &codigo);
     }
     fclose(dados);
+    
     puts("\nCodigo:");
     imprimeAVL(raizAVL);
     puts("\nNome:");
     imprime234(raiz234);
     puts("\n");
+    
+    FILE *arqBuscas = fopen(buscas, "r");
+    if(arqBuscas == NULL){
+        exit(-1);
+    }
+    char tipo;
+    fscanf(arqBuscas, "%c", &tipo);
+    while(tipo != 'f'){
+        if(tipo == 'c'){
+            fscanf(arqBuscas,"%d", &codigo);
+            printf("%d\n", codigo);
+            ApAVL no = buscaAVL(raizAVL, codigo);
+            if(no != NULL){
+                puts("Achou");
+                // TODO: Recuperar linha do arquivo
+            }
+        }else if(tipo == 'n'){
+            fscanf(arqBuscas,"%s", &jogo);
+            printf("%s\n", &jogo);
+            // TODO: Buscar e recuperar linha do arquivo.
+        }
+        fscanf(arqBuscas, "%c", &tipo);
+    }
+    
+    fclose(arqBuscas);
+    
 
     exit(0);
 }
